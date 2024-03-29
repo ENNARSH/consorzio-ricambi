db36f6a6253f715cabd8ffab700e0870973bfa04
ennarsh <gennaro.liguori.sh@gmail.com>
2024-03-29 19:01:49 +0100

    cors policy

--- profumi-shop/src/main/java/com/beautyandhome/profumishop/config/CorsConfig.java
+++ profumi-shop/src/main/java/com/beautyandhome/profumishop/config/CorsConfig.java
@@ -1,2 +1,20 @@
-package com.beautyandhome.profumishop.config;public class CorsConfig {
+package com.beautyandhome.profumishop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // Specifica gli origini consentiti
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
+
