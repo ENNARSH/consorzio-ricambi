6dee65621203e6256dab78ba84751965a18fbe32
ennarsh <gennaro.liguori.sh@gmail.com>
2024-04-06 18:21:29 +0200

    feat: content type test

--- profumi-shop/src/main/java/com/beautyandhome/profumishop/controller/ProdottoController.java
+++ profumi-shop/src/main/java/com/beautyandhome/profumishop/controller/ProdottoController.java
@@ -6,14 +6,13 @@ import com.beautyandhome.profumishop.model.Prodotto;
 import com.beautyandhome.profumishop.service.ProdottoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/prodotti")
+@CrossOrigin(origins = "https://ennarsh.github.io") // Allow requests from specific origin
 public class ProdottoController {
 
     private final ProdottoService prodottoService;
@@ -24,18 +23,20 @@ public class ProdottoController {
     }
 
     @GetMapping
-    public List<Prodotto> getAllProdotti() {
-        return prodottoService.getAllProdotti();
+    public ResponseEntity<List<Prodotto>> getAllProdotti() {
+        List<Prodotto> prodotti = prodottoService.getAllProdotti();
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(prodotti);
     }
 
     @GetMapping("/filtered")
     public ResponseEntity<List<Prodotto>> getAllProdottiFiltered(@RequestParam String categoria) {
         List<Prodotto> prodotti = prodottoService.getAllProdottiFiltered(categoria);
-        return new ResponseEntity<>(prodotti, HttpStatus.OK);
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(prodotti);
     }
 
     @GetMapping("/details")
-    public List<Prodotto> getProdottoById(String id) {
-        return prodottoService.getProdottoById(id);
+    public ResponseEntity<List<Prodotto>> getProdottoById(@RequestParam String id) {
+        List<Prodotto>  prodotto = prodottoService.getProdottoById(id);
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(prodotto);
     }
 }
